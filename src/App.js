b741f45acc16181cca442209fd25f5da5ca1dc18
junggri <jjuu6933@naver.com>
2020-09-24 23:00:03 +0900

    react practice

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import PhoneForm from "./component/PhoneForm";
+import PhoneInfoList from "./component/PhoneInfoList";
 import "./App.css";
 
-class App extends React.Component {
+class App extends Component {
+  id = 2;
+  state = {
+    information: [
+      {
+        id: 0,
+        name: "김민준",
+        phone: "010-0000-0000",
+      },
+      {
+        id: 1,
+        name: "홍길동",
+        phone: "010-0000-0001",
+      },
+    ],
+  };
   handleCreate = (data) => {
-    console.log(data);
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data }),
+    });
   };
   render() {
     return (
       <div>
         <PhoneForm onCreate={this.handleCreate} />
+        <PhoneInfoList data={this.state.information} />
       </div>
     );
   }
