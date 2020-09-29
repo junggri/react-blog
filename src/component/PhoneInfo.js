11fb376ace8d7fc53d01f93efbedf8e62974d27e
junggri <jjuu6933@naver.com>
2020-09-30 00:31:01 +0900

    react is hard

--- src/component/PhoneInfo.js
+++ src/component/PhoneInfo.js
@@ -8,15 +8,65 @@ class PhoneInfo extends Component {
       id: 0,
     },
   };
+  state = {
+    editing: false,
+    name: "",
+    phone: "",
+  };
 
+  handleRemove = () => {
+    const { info, onRemove } = this.props;
+    onRemove(info.id);
+  };
+  handelToggleEdit = () => {
+    const { editing } = this.state;
+    this.setState({
+      editing: !editing,
+    });
+  };
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+  componentDidUpdate(prevProps, prevState) {
+    const { info, onUpdate } = this.props;
+    if (!prevState.editing && this.state.editing) {
+      this.setState({
+        name: info.name,
+        phone: info.phone,
+      });
+    }
+    if (!prevState.editing && !this.state.editing) {
+      onUpdate(info, {
+        name: this.state.name,
+        phone: this.state.phone,
+      });
+    }
+  }
   render() {
     const style = {
       border: "1px solid black",
       padding: "8px",
       margin: "8px",
     };
-
-    const { name, phone, id } = this.props.info;
+    const { editing } = this.state;
+    if (editing) {
+      return (
+        <div style={style}>
+          <div>
+            <input value={this.state.name} name="name" placeholder="이름" onChange={this.handleChange} />
+          </div>
+          <div>
+            <input value={this.state.props} name="phone" placeholder="전화번허" onChange={this.handleChange} />
+          </div>
+          <button onClick={this.handelToggleEdit}>적용</button>
+          <button onClick={this.handleRemove}>삭제</button>
+        </div>
+      );
+    }
+    const { name, phone } = this.props.info;
 
     return (
       <div style={style}>
@@ -24,6 +74,8 @@ class PhoneInfo extends Component {
           <b>{name}</b>
         </div>
         <div>{phone}</div>
+        <button onClick={this.handleToggleEdit}>수정</button>
+        <button onClick={this.handleRemove}>삭제</button>
       </div>
     );
   }
